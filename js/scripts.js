75a4657de66762e23f54cde8e3192c3981ba8b7a
Brandon Betts and Devin Mounts <devin@>
2018-05-30 14:17:48 -0700

    add button with img that is removed with click.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,24 @@
 $(document).ready(function() {
-  $("button#hello").click(function() {
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>What's Up!</li>")
-
-    $("ul#user").children("li").first().click(function(){
-      (this).remove();
+  $("button#cat").click(function() {
+    $("ul#left").prepend("<li>MEEEEEEEW</li>");
+    $("ul#right").prepend("<li>REEEEEEE</li>");
+    $("ul#left").children("li").first().click(function() {
+      $(this).remove();
     });
-
-    $("ul#webpage").children("li").first().click(function(){
-      (this).remove();
+    $("ul#right").children("li").first().click(function() {
+      $(this).remove();
     });
   });
 
-  $("button#goodbye").click(function() {
-    $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Adios!</li>")
-
-    $("ul#user").children("li").first().click(function(){
-      (this).remove();
-    });
-
-    $("ul#webpage").children("li").first().click(function(){
-      (this).remove();
-    });
+  $("button#dog").click(function() {
+    $("ul#right").prepend("<li>REEEEEEE</li>");
+    $("ul#left").prepend("<li>MEEEEEEEW</li>");
   });
 
-  $("button#stop").click(function() {
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Woops</li>")
-
-    $("ul#user").children("li").first().click(function(){
-      (this).remove();
-    });
-
-    $("ul#webpage").children("li").first().click(function(){
-      (this).remove();
+  $("button#crazydog").click(function() {
+    $("ul#doglist").prepend("<li><img src='img/woof.jpg'></li>");
+    $("ul#doglist").children("li").first().click(function(){
+      $(this).remove();
     });
   });
-
-
 });
